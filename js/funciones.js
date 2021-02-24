f0223aa04c6d9ad954ed2e07136c70bec3e196b0
Santi Arnau <santi07a@gmail.com>
2021-02-24 20:39:12 +0100

    agregado funcion 1

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,6 @@
 // Funcion 1
-function equiposMayoresEdad(equipos, edad) {
 
-}
+const equiposMayoresDeEdad = equipos => equipos.filter(equipo => equipo.asignado.empleado.edad >= 18).length;
 
 // Funcion 2
 function equiposProvincia(equipos, provincias) {
@@ -12,8 +11,9 @@ function provincias(equipos) {
 
 }
 // Funcion 4
-const puestos = equipos.map(encargo => encargo.equipos.asignado.empleado.puesto)
+const puestos = equipos.map(encargo => encargo.equipos.asignado.empleado.puesto);
 
 // Funcion 5
-const edadMedia = equipos.reduce((accu, elemento) => accum + elemento.equipos.asignado.empleado.edad, 0) / equipos.length;
+const edadMedia = equipos.reduce((accu, elemento) => accu + elemento.equipos.asignado.empleado.edad, 0) / equipos.length;
+console.log(edadMedia);
 // Funcion 6
