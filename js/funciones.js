2ad475df9ca835897eef116c58d44a21219814a8
mikicastro69 <castro-miquel@hotmail.com>
2021-02-25 10:52:36 +0100

    Creo las funciones de la 7 a 11

--- js/funciones.js
+++ js/funciones.js
@@ -15,8 +15,31 @@ function provincias(equipos) {
 //const edadMedia = equipos.reduce((accu, elemento) => accu + elemento.equipos.asignado.empleado.edad, 0) / equipos.length;
 
 // Funcion 6
-const equiposPorEdad = equipos => equipos.sort((a, b) => a.asignado.empleado.edad - b.asignado.empleado.edad)
+const equiposPorEdad = equipos => equipos.sort((a, b) => a.asignado.empleado.edad - b.asignado.empleado.edad);
 
 // Funcion 7
-const equiposTipo = ((equipos, tipo) => equipos.filter(equipo => equipo.tipo.toLowerCase() === tipo.toLowerCase()))
-console.log(equiposTipo(equipos, "sobremesa"))
+const equiposTipo = ((equipos, tipo) =>
+  equipos.filter(equipo => equipo.tipo.toLowerCase() === tipo.toLowerCase()));
+
+// Funcion 8
+const trabajadoresTipo = ((equipos, tipo) => equipos.filter(equipo => equipo.tipo.toLowerCase() === tipo.toLowerCase())
+  .map(equipo => {
+    const trabajador = {
+      empleado: equipo.asignado.empleado
+    }
+    return trabajador
+  }));
+
+// Funcion 10
+const equiposTipoLocalidad = ((equipos, tipo, localidad) =>
+  equipos.filter(equipo => equipo.tipo.toLowerCase() === tipo.toLowerCase() && equipo.asignado.poblacion.toLowerCase() === localidad.toLowerCase()));
+
+//Funcion 11
+const resumenEquipos = equipos => equipos.map(equipo => {
+  const resumen = {
+    id : equipo.id,
+    poblacion: equipo.asignado.poblacion,
+    provincia: equipo.asignado.provincia
+  }
+  return resumen;
+})
