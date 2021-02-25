f786170982aa75e85b4b4758d620033484fdb6ef
Santi Arnau <santi07a@gmail.com>
2021-02-25 10:10:12 +0100

    cambio de estilo al recuadro gris

--- js/funciones.js
+++ js/funciones.js
@@ -9,14 +9,16 @@ function provincias(equipos) {
 
 }
 // Funcion 4
-//const puestos = equipos.map(encargo => encargo.equipos.asignado.empleado.puesto);
+// const puestos = equipos.map(encargo => encargo.equipos.asignado.empleado.puesto);
 
 // Funcion 5
-//const edadMedia = equipos.reduce((accu, elemento) => accu + elemento.equipos.asignado.empleado.edad, 0) / equipos.length;
+// const edadMedia = equipos.reduce((accu, elemento) => accu + elemento.equipos.asignado.empleado.edad, 0) / equipos.length;
 
 // Funcion 6
-const equiposPorEdad = equipos => equipos.sort((a, b) => a.asignado.empleado.edad - b.asignado.empleado.edad)
+const equiposPorEdad = equipos => equipos.sort((a, b) => a.asignado.empleado.edad - b.asignado.empleado.edad);
 
 // Funcion 7
-const equiposTipo = ((equipos, tipo) => equipos.filter(equipo => equipo.tipo.toLowerCase() === tipo.toLowerCase()))
-console.log(equiposTipo(equipos, "sobremesa"))
+const equiposTipo = ((equipos, tipo) => equipos.filter(equipo => equipo.tipo.toLowerCase() === tipo.toLowerCase()));
+console.log(equiposTipo(equipos, "sobremesa"));
+
+// Funcion 9
